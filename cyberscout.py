3f1f93390d501eb46d36064c96e85b969df35fa6
0c1av <pirate.anonymusli@gmail.com>
2024-11-11 17:10:49 +0100

    Update cyberscout.py

--- cyberscout.py
+++ cyberscout.py
@@ -6,6 +6,7 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime
 import subprocess
 import random
+import sys
 
 #functions
 def get_proxy_with_api():
@@ -16,13 +17,13 @@ def get_proxy_with_api():
 			if proxy_list:
 				return random.choice(proxy_list)
 			else:
-				print(f"{ORANGE}[WARNING] No proxies returned by the API.{RESET}")
+				print(f"{ORANGE}[WARNING]{RESET} No proxies returned by the API.")
 				return None
 		else:
-			print(f"[ERROR] Failed to fetch proxies from the API.{RESET}")
+			print(f"{RED}[ERROR]{RESET} Failed to fetch proxies from the API.")
 			return None
 	except requests.exceptions.RequestException as e:
-		print(f"{RED}[ERROR] Unable to fetch proxy: {e}{RESET}")
+		print(f"{RED}[ERROR]{RESET} Unable to fetch proxy: {e}")
 		return None
 
 def timeout_calc(timeout_status, timeout_freq):
@@ -82,7 +83,7 @@ def advice_calc(timeout_freq, forbidden_freq, found_freq, error_freq, proxy_opti
 	'''timeout'''
 	if timeout_freq > 2:
 		advice_list.append("Try increasing the timeout")
-	if timeout_freq > 4:
+	if timeout_freq > 6:
 		if proxy_option == False:
 			advice_list.append("Try using a proxy")
 		if proxy_option == True:
@@ -104,7 +105,7 @@ def advice_calc(timeout_freq, forbidden_freq, found_freq, error_freq, proxy_opti
 			advice_list.append("There could be a problem with the target. Try restarting the program")
 
 	'''error + ...'''
-	if (error_freq + timeout_freq) > 3:
+	if (error_freq + timeout_freq) > 4:
 		if proxy_option == True:
 			advice_list.append("There could be a problem with the proxy. Try restarting the program")
 		elif proxy_option == False:
@@ -121,6 +122,7 @@ parser.add_argument("-t", "--timeout", type=int, default=5, help="Timeout for re
 parser.add_argument("-o", "--output", type=str, help="File to save the output (e.g. results.txt)")
 parser.add_argument("-a", "--auth", type=str, help="Basic authentication in the format 'username:password'")
 parser.add_argument("-x", "--proxy", nargs='?', const="built_in", help="Proxy to use in the format 'ip:port'. If omitted, a built-in proxy will be used.")
+parser.add_argument("-m", "--method", type=str, default="GET", help="HTTP method to use (GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH)")
 args = parser.parse_args()
 
 #colors
@@ -138,6 +140,13 @@ url = args.url
 path_file = args.wordlist
 timeout = args.timeout
 output_file = args.output
+if output_file == None:
+	pass
+elif os.path.exists(output_file):
+	print(f"{RED}[ERROR]{RESET}The given output file already exists")
+	sys.exit()
+method = args.method
+valid_methods = ['get', 'post', 'put', 'delete', 'head', 'options', 'patch']
 timeout_status = False
 timeout_freq = 0
 forbidden_status = False
@@ -156,6 +165,12 @@ auth = None
 if args.auth:
         username, password = args.auth.split(":")
         auth = HTTPBasicAuth(username, password)
+
+if url.startswith("https://") or url.startswith("http://"):
+	pass
+else:
+	print(f"{RED}[ERROR]{RESET} Invalid protocol, url must start with 'http(s)://'")
+	sys.exit()
 
 #proxy
 proxy_option = False
@@ -182,6 +197,7 @@ print(f"Url:        {url}")
 print(f"Wordlist:   {path_file}")
 print(f"Timeout:    {timeout}")
 print(f"Proxy:      {proxies}")
+print(f"Method:     {method.upper()}")
 print("========================================================")
 print(f"{current_time}: DirHunter launched by {user}")
 print("========================================================")
@@ -189,65 +205,99 @@ print("")
 
 
 #Trying paths
-if output_file:
-	output_handle = open(output_file, 'w')
+if output_file is not None:
+	with open(output_file, 'w') as file:
+		file.write("========================================================\n")
+
 
 with open(path_file, 'r') as file:
 	paths_list = file.readlines()
 
-for path in paths_list:
-	path = path.strip()
-	full_url = f"{url}/{path}"
-	try:
-		response = requests.get(full_url, timeout=timeout, auth=auth, proxies=proxies)
-		if response.status_code == 200:
-			result = f"{GREEN}[FOUND]{RESET} {full_url}"
-			timeout_status = False
-			forbidden_status = False
-			found_status = True
-			error_status = False
-		elif response.status_code == 404:
-			result = f"[NOT FOUND] {full_url}"
-			timeout_status = False
+found_list = []
+
+try:
+	for path in paths_list:
+		path = path.strip()
+		full_url = f"{url}/{path}"
+
+		try:
+			if method.lower() in valid_methods:
+				response = getattr(requests, method.lower())(full_url, timeout=timeout, auth=auth, proxies=proxies)
+			else:
+				print(f"{RED}[ERROR]{RESET} Invalid method")
+				sys.exit()
+
+			if response.status_code == 200:
+				result = f"{GREEN}[FOUND]{RESET} {full_url}"
+				timeout_status = False
+				forbidden_status = False
+				found_status = True
+				error_status = False
+				found_list.append(full_url)
+
+				if output_file:
+					with open(output_file, 'r+') as file:
+						content = file.readlines()
+						for i, line in enumerate(content):
+							if line.strip() == "========================================================":
+								content.insert(i, f"{GREEN}[FOUND]{RESET} {full_url}\n")
+								break
+						file.seek(0)
+						file.writelines(content)
+
+			elif response.status_code == 404:
+				result = f"[NOT FOUND] {full_url}"
+				timeout_status = False
+				forbidden_status = False
+				found_status = False
+				error_status = False
+
+			elif response.status_code == 403:
+				result = f"{D_GRAY}[FORBIDDEN]{RESET} {full_url}"
+				timeout_status = False
+				forbidden_status = True
+				found_status = False
+				error_status = False
+			else:
+				result = f"[{response.status_code}] {full_url}"
+				timeout_status = False
+				forbidden_status = False
+				found_status = False
+				error_status = False
+
+		except requests.exceptions.Timeout:
+			result = f"{L_GRAY}[TIMEOUT]{RESET} {full_url}"
+			timeout_status = True
 			forbidden_status = False
 			found_status = False
 			error_status = False
-		elif response.status_code == 403:
-			result = f"{D_GRAY}[FORBIDDEN]{RESET} {full_url}"
-			timeout_status = False
-			forbidden_status = True
-			found_status = False
-			error_status = False
-		else:
-			result = f"[{response.status_code}] {full_url}"
+
+		except requests.exceptions.RequestException as e:
+			result = f"{RED}[ERROR]{RESET} {full_url} -> {e}"
 			timeout_status = False
 			forbidden_status = False
 			found_status = False
-			error_status = False
+			error_status = True
 
-	except requests.exceptions.Timeout:
-		result = f"{L_GRAY}[TIMEOUT]{RESET} {full_url}"
-		timeout_status = True
-		forbidden_status = False
-		found_status = False
-		error_status = False
+		print(result)
+		if output_file:
+			with open(output_file, 'a') as file:
+				file.write(f"{result}\n")
 
-	except requests.exceptions.RequestException as e:
-		result = f"{RED}[ERROR]{RESET} {full_url} -> {e}"
-		timeout_status = False
-		forbidden_status = False
-		found_status = False
-		error_status = True
-
-	print(result)
-
-	if output_file:
-		output_handle.write(result + '\n')
 	timeout_freq = timeout_calc(timeout_status, timeout_freq)
 	forbidden_freq = forbidden_calc(forbidden_status, forbidden_freq)
 	found_freq = found_calc(found_status, found_freq)
 	error_freq = error_calc(error_status, error_freq)
 	advice_calc(timeout_freq, forbidden_freq, found_freq, error_freq, proxy_option)
 
-if output_file:
-	output_handle.close()
+	print("========================================================")
+	print("Found URLs:\n")
+	for found in found_list:
+		print(found)
+except KeyboardInterrupt:
+	print("Keyborad interrumpt\n")
+	print("========================================================\n")
+	print("Found URLs:")
+	for found in found_list:
+		print(found)
+	sys.exit(0)
